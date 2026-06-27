ae92599fe33e5835b77078f782230f320956b09b
Maria Marta <liacode19@gmail.com>
2026-06-27 09:53:16 -0300

    chore(project): estrutura inicial do backend Spring Boot

---
+++ src/main/java/br/com/reireal/ReirealBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.reireal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReirealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReirealBackendApplication.class, args);
+	}
+
+}
